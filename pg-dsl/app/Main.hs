eaf79b645691fd788a56483f10e4aa0da6b6822c
stuebema <stueben@isse.de>
2024-05-15 17:32:37 +0200

    Use shell instead of proc to launch pg-verify, due to Windows

--- pg-dsl/app/Main.hs
+++ pg-dsl/app/Main.hs
@@ -19,7 +19,7 @@ import           System.Environment        (getArgs, withArgs)
 import           System.Exit               (ExitCode (ExitFailure), exitSuccess,
                                             exitWith)
 import           System.FilePath           (takeDirectory, (</>))
-import           System.Process            (createProcess, proc, waitForProcess)
+import           System.Process            (createProcess, proc, shell, waitForProcess)
 import           Tokenizer                 (tokenize)
 import           TypeCheck                 (checkTypes)
 import           System.Console.CmdArgs
@@ -60,34 +60,34 @@ hasMain basepath = do
 cmdHandler :: PgDSL -> String -> IO ()
 cmdHandler (Test pgDir) jsonFile = do
   putStrLn $ "Testing program graphs from folder " ++ pgDir
-  let cmd = ["test"]
+  let cmd = "test"
   let jsonArgs = ["--json-file", jsonFile]
-  (_, _, _, processHandle) <- createProcess (proc "pg-verify" $ cmd ++ jsonArgs)
+  (_, _, _, processHandle) <- createProcess (shell $ "pg-verify " ++ cmd ++ " " ++ (unwords jsonArgs))
   _ <- waitForProcess processHandle
   putStrLn ""
 cmdHandler (Simulate pgDir steps withFaults) jsonFile = do
   let s = if (steps > 1) then steps else 10
   putStrLn $ "Simulating program graphs from folder " ++ pgDir ++ " for " ++ (show s) ++ " steps."
   putStrLn $ "Faults are " ++ (if withFaults then "included." else "excluded.")
-  let cmd = ["simulate"]
+  let cmd = "simulate"
   let jsonArgs = ["--json-file", jsonFile]
   let stepsArg = ["--steps", show s]
   let faultsArg = if withFaults then ["--with-faults"] else []
-  (_, _, _, processHandle) <- createProcess (proc "pg-verify" $ cmd ++ jsonArgs ++ stepsArg ++ faultsArg)
+  (_, _, _, processHandle) <- createProcess (shell $ "pg-verify " ++ cmd ++ " " ++ (unwords $ jsonArgs ++ stepsArg ++ faultsArg))
   _ <- waitForProcess processHandle
   putStrLn ""
 cmdHandler (DCCA pgDir) jsonFile = do
   putStrLn $ "Performing DCCA for program graphs from folder " ++ pgDir
-  let cmd = ["dcca"]
+  let cmd = "dcca"
   let jsonArgs = ["--json-file", jsonFile]
-  (_, _, _, processHandle) <- createProcess (proc "pg-verify" $ cmd ++ jsonArgs)
+  (_, _, _, processHandle) <- createProcess (shell $ "pg-verify " ++ cmd ++ " " ++ (unwords jsonArgs))
   _ <- waitForProcess processHandle
   putStrLn ""
 cmdHandler (Output pgDir typ) jsonFile = do
   putStrLn $ "Showing program graphs from folder " ++ pgDir ++ " as " ++ (show typ)
-  let cmd = ["show", map toLower (show typ)]
+  let cmd = (unwords ["show", map toLower (show typ)])
   let jsonArgs = ["--json-file", jsonFile]
-  (_, _, _, processHandle) <- createProcess (proc "pg-verify" $ cmd ++ jsonArgs)
+  (_, _, _, processHandle) <- createProcess (shell $ "pg-verify " ++ cmd  ++ " " ++ (unwords jsonArgs))
   _ <- waitForProcess processHandle
   putStrLn ""
 
